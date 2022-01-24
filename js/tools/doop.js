e479048814625c903fcdb79883f6056d6a4230c9
bucketteOfIvy <ashewimer@gmail.com>
2022-01-24 16:34:11 -0600

    fixed comment -- C2D != Canvas API

--- js/tools/doop.js
+++ js/tools/doop.js
@@ -9,7 +9,7 @@ window.tools.doop = {
   events: {
     click: function (e) {
       if (window.tools.doop.state.selected) {
-        // borrowed + very mildly edited from C2D example docs
+        // borrowed + very mildly edited from canvas example docs
         const mouse = C2D.eventToMouse(e)
         C2D.ctx.font = '48px serif'
         C2D.ctx.stroketext('doop!', mouse.x, mouse.y)
