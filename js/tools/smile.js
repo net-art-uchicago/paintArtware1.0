c50da03ea952d598ddd7b718957c7762efae4cb1
silviachie <88081257+silviachie@users.noreply.github.com>
2022-01-26 22:19:51 -0600

    smile face

---
+++ js/tools/smile.js
@@ -0,0 +1,35 @@
+window.tools.smile = {
+    name: ':)',
+    icon: '/images/smile-icon.png',
+    state: {
+      selected: false,
+      mousePressed: false,
+      //prevMouse: { x: null, y: null}
+    },
+    events: {
+      mousedown: function () {
+          const state = windows.tools.smile.state
+          state.mousePressed = true
+      },
+      mouseup: function () {
+          const state = window.tools.smile.state
+          state.mousePressed = false
+          state.mousePressed = true
+      },
+      mousemove: function (e) {
+          const state = window.tools.smile.state
+          if (state.selected && state.mousePressed) {
+            const mouse = C2D.eventToMouse(e)
+            C2D.ellipse(mouse.x, mouse.y, size)
+            C2D.ellipse(mouse.x + (size/4), mouse.y + (size/4), size)
+            C2D.ellipse(mouse.x - (size/4), mouse.y + (size/4), size/5)
+            C2D.ellipse(mouse.x - (size/4), mouse.y + (size/4), size/5)
+            C2D.ellipse(mouse.x , mouse.y - (size/4), size/2)
+            C2D.rectangle(mouse.x, mouse.y - (size/6), size/2)
+
+          }
+      }
+    }
+  }
+ 
+
