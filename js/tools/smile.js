1aba38a3b9cf04d6c94369d1c66cd8b2a699e1ed
silviachie <88081257+silviachie@users.noreply.github.com>
2022-01-26 22:41:07 -0600

    circle

--- js/tools/smile.js
+++ js/tools/smile.js
@@ -19,7 +19,15 @@ window.tools.smile = {
           const state = window.tools.smile.state
           if (state.selected && state.mousePressed) {
             const mouse = C2D.eventToMouse(e)
-            C2D.ellipse(mouse.x, mouse.y, size)
+            const time = Date.now()
+        const speed = 0.01
+        const scale = 10
+        const oscilation = Math.sin(time * speed) * scale
+        const size = Math.abs(oscilation)
+        const mouse = C2D.eventToMouse(e)
+
+        C2D.ellipse(mouse.x, mouse.y, size)
+            //C2D.ellipse(mouse.x, mouse.y, size)
             // C2D.ellipse(mouse.x + (size/4), mouse.y + (size/4), size)
             // C2D.ellipse(mouse.x - (size/4), mouse.y + (size/4), size/5)
             // C2D.ellipse(mouse.x - (size/4), mouse.y + (size/4), size/5)
