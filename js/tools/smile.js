0fa53eb377e952a3a7073d0cfab2de58f3b55575
silviachie <88081257+silviachie@users.noreply.github.com>
2022-01-26 23:19:10 -0600

    eyes

--- js/tools/smile.js
+++ js/tools/smile.js
@@ -19,8 +19,8 @@ window.tools.smile = {
           if (state.selected && state.mousePressed) {
             const mouse = C2D.eventToMouse(e)
             C2D.ellipse(mouse.x, mouse.y, 100)
-            C2D.ellipse(mouse.x + (100/4), mouse.y - (100/3), 100/10)
-            C2D.ellipse(mouse.x - (100/4), mouse.y - (100/3), 100/10)
+            C2D.ellipse_full(mouse.x + (100/3), mouse.y - (100/3), 100/10)
+            C2D.ellipse_full(mouse.x - (100/3), mouse.y - (100/3), 100/10)
             C2D.ellipse(mouse.x , mouse.y + (100/3), 100/2)
             C2D.rect(mouse.x, mouse.y - (100/6), 100/2)
           }
