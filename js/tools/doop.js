544970bef71b502a33afd0de56de37ea92c45dee
bucketteOfIvy <ashewimer@gmail.com>
2022-01-28 22:29:49 -0600

    added click and drag "doop!" tool. (doop doop doop!)

--- js/tools/doop.js
+++ js/tools/doop.js
@@ -4,15 +4,25 @@ window.tools.doop = {
   icon: '/images/app-icon.png',
   state: {
     selected: false,
-    isWorking: false // sad :(
+    mousePressed: false
   },
   events: {
-    click: function (e) {
-      if (window.tools.doop.state.selected) {
-        // borrowed + very mildly edited from canvas example docs
+    mousedown: function () {
+      const state = window.tools.doop.state
+      state.mousePressed = true
+    },
+    mouseup: function () {
+      const state = window.tools.doop.state
+      state.mousePressed = false
+    },
+    mousemove: function (e) {
+      const state = window.tools.doop.state
+      if (state.selected && state.mousePressed) {
         const mouse = C2D.eventToMouse(e)
         C2D.ctx.font = '48px serif'
-        C2D.ctx.stroketext('doop!', mouse.x, mouse.y)
+        C2D.ctx.textAlign = 'center'
+        C2D.ctx.textBaseline = 'middle'
+        C2D.ctx.fillText('doop!', mouse.x, mouse.y)
       }
     }
   }
