48ba1cb9e768d6002a855701e7f8bc200ebfe2a5
silviachie <88081257+silviachie@users.noreply.github.com>
2022-01-31 15:02:56 -0600

    circles

--- js/tools/smile.js
+++ js/tools/smile.js
@@ -43,8 +43,8 @@ window.tools.smile = {
            
             C2D.fill = features0
             C2D.stroke = skin0
-            C2D.ellipse(mouse.x + (100/3), mouse.y - 20, 100/9)
-            C2D.ellipse(mouse.x - (100/3), mouse.y - 20, 100/9)
+            C2D.ellipse(mouse.x + (100/3), mouse.y - 20, 100/5, 100/10)
+            C2D.ellipse(mouse.x - (100/3), mouse.y - 20, 100/5, 100/10)
             //C2D.rect(mouse.x - 100/5, mouse.y - 100/5, mouse.x+ 100/3, mouse.y + 100/3) 
             
             C2D.fill = features0
