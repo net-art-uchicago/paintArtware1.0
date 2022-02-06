d50a9e88d6b6eeea8dd3c8deb2bdd45bfd8b1b30
Ashley <Ashleydai@uchicago.edu>
2022-02-05 18:22:38 -0600

    fixing errors added comment

---
+++ js/tools/airBrush.js
@@ -0,0 +1,31 @@
+/* global C2D */
+window.tools.airBrush = {
+  name: 'airbrush',
+  icon: '/images/airbrush',
+  state: {
+    selected: false,
+    mousePressed: false,
+    prevMouse: { x: null, y: null }
+  },
+  events: {
+    mousedown: function () {
+      const state = window.tools.airBrush.state
+      state.mousePressed = true
+    },
+    mouseup: function () {
+      const state = window.tools.airBrush.state
+      state.mousePressed = false
+      state.prevMouse = { x: null, y: null }
+    },
+    mousemove: function (e) {
+      const state = window.tools.airBrush.state
+      if (state.selected && state.mousePressed) {
+        const mouse = C2D.eventToMouse(e)
+        const px = state.prevMouse.x || mouse.x
+        const py = state.prevMouse.y || mouse.y
+        C2D.line(mouse.x, mouse.y, px, py)
+        state.prevMouse = { x: mouse.x, y: mouse.y }
+      }
+    }
+  }
+}
