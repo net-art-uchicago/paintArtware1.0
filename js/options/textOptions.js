2a376e2fcba936e6c34bb2835a1db5aac4bf13db
bucketteOfIvy <ashewimer@gmail.com>
2022-02-07 15:50:18 -0600

    culled options, made css look sufficiently okay and not be too glitchy

--- js/options/textOptions.js
+++ js/options/textOptions.js
@@ -10,73 +10,44 @@ window.options.textOptions = {
     const div = document.createElement('div')
     div.innerHTML = `
       <style>
-
-        input {
-          maxlegth: 30;
-          size: 15;
-        }
-
-        input, select {
-          width: 125px;
-        }
-
        .text-options {
           display: grid;
-          grid-template-columns: 200px 200px 200 px 200px;
-          grid-template-rows: auto;
+          grid-template-columns: 450px;
+          grid-template-rows: 1fr 1fr;
           grid-template-areas:
-           "wordlab word fontSizeLab fontSize"
-           "fontFamilyLab fontFamily fontAlignLab fontAlign"
-           ". fontBaselineLab fontBaseline .";
+          "top"
+          "bottom";
         }
 
-        label.input-text { grid-area: wordlab; }
-        input.input-text { grid-area: word; }
-        label.font-sizer { grid-area: fontSizeLab; }
-        input.font-sizer { grid-area: fontSize; }
+        div > div {
+          display: flex;
+        }
+
+        div:nthchild(1) {
+          grid-area: top;
+        }
+
+        div:nchild(2) {
+          grid-area: bottom;
+        }
+        label.input-text {
+          margin-right: 50px;
+        }
+        label.font-sizer {
+          width: 150px;
+          margin-right: 0px;
+        }
 
       </style>
-
       <div class="text-options">
-        <label for="input-text" class="input-text"> What to Write:  </label>
-        <input value=${state.text} type="text" id="input-text" class="input-text">
-      </div>
-      <div class="text-options">
-        <label for="font-sizer" class="font-sizer"> Font Size: ${C2D.fontSize}</label>
-        <input type="range" id="font-sizer" class="font-sizer" min="1" max="128" value="${C2D.fontSize}px">
-      </div>
-      <div class="text-options">
-        <label for="font-family" class="font-family">Font Family</label>
-        <select id="font-family" class="font-family">
-          <option value="Serif">Serif</option>
-          <option value="Verdana">Verdana</option>
-          <option value="Helvetica">Helvetica</option>
-          <option value="Georgia">Georgia</option>
-          <option value="Garamond">Garamond</option>
-          <option value="Courier New">Courier New</option>
-          <option value="Brush Script MT">Brush Script MT</option>
-        </select>
-      </div>
-      <div class="text-options">
-        <label for="font-align" class="font-align">Font Align</label>
-        <select id="font-align" class="font-align">
-          <option value="center">center</option>
-          <option value="start">start</option>
-          <option value="left">left</option>
-          <option value="right">right</option>
-          <option value="end">end</option>
-        </select>
-      </div>
-      <div class="text-options">
-        <label for="font-baseline" class="font-baseline">Font Baseline</label>
-        <select id="font-baseline" class="font-baseline">
-          <option value="middle">Middle</option>
-          <option value="alphabetic">Alphabetic</option>
-          <option value="top">Top</option>
-          <option value="hanging">Hanging</option>
-          <option value="bottom">Bottom</option>
-          <option value="ideographic">Ideographic</option>
-        </select>
+        <div>
+          <label for="input-text" class="input-text"> What to Write:  </label>
+          <input value=${state.text} type="text" id="input-text" class="input-text">
+        </div>
+        <div>
+          <label for="font-sizer" class="font-sizer"> Font Size: ${C2D.fontSize}</label>
+          <input type="range" id="font-sizer" class="font-sizer" min="1" max="128" value="${C2D.fontSize}px">
+        </div>
       </div>
     `
 
@@ -94,21 +65,6 @@ window.options.textOptions = {
       const inputFontSize = e.target.value
       C2D.fontSize = inputFontSize
       sizeLabel.textContent = `Font Size: ${C2D.fontSize}`
-    })
-
-    const familySelect = div.querySelector('select[id="font-family"]')
-    familySelect.addEventListener('input', (e) => {
-      C2D.fontFamily = familySelect.value
-    })
-
-    const fontAlign = div.querySelector('select[id="font-align"]')
-    fontAlign.addEventListener('input', (e) => {
-      C2D.fontAlign = fontAlign.value
-    })
-
-    const fontBaseline = div.querySelector('select[id="font-baseline"]')
-    fontBaseline.addEventListener('input', (e) => {
-      C2D.fontBaseline = fontBaseline.value
     })
 
     return div
