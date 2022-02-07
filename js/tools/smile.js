e403cb863b01ef737f93f8e1cc5cfde6b92b749b
silviachie <88081257+silviachie@users.noreply.github.com>
2022-02-07 13:41:38 -0600

    added borders to face

--- js/tools/smile.js
+++ js/tools/smile.js
@@ -19,46 +19,31 @@ window.tools.smile = {
 
           if (state.selected && state.mousePressed) {
             const mouse = C2D.eventToMouse(e)
+
             fill1 = C2D.fill
             stroke1 = C2D.stroke
             width = (C2D.ctx.lineWidth)*5
+
+            C2D.change_colors(fill1,fill1)
+            C2D.ellipse(mouse.x, mouse.y, width * 1.05)
+
             C2D.change_colors(stroke1,stroke1)
             C2D.ellipse(mouse.x, mouse.y, width) //face
+
             C2D.change_colors(fill1,fill1)
-            C2D.ellipse(mouse.x , mouse.y + (width/5), width/1.8, width/1.9) // innermouth
+            C2D.ellipse(mouse.x , mouse.y + (width/5), width/1.8, width/1.9) // inner mouth
+            
             C2D.change_colors(stroke1,stroke1)
             C2D.ellipse(mouse.x , mouse.y, (width*.7), (width * 0.4)) //outer mouth
+            
             C2D.change_colors(fill1,fill1)
             C2D.ellipse(mouse.x + (width/2.75), mouse.y - (width/5), width/13, width/7) //eyes
             C2D.ellipse(mouse.x - (width/2.75), mouse.y - (width/5), width/13, width/7)
+            
             C2D.change_colors(stroke1,fill1)
             
           }
         }
       }
     }
-    
-
-//     mousemove: function (e) {
-//       const state = window.tools.smile.state
-//       if (state.selected && state.mousePressed) {
-//         const mouse = C2D.eventToMouse(e)
-//         skin = C2D_fill
-//         // o_stroke = C2D.stroke
-//        // switch_colors()
-//         C2D.ellipse(mouse.x, mouse.y, 100)
-//         // switch_colors()
-//         // C2D.ellipse(mouse.x , mouse.y + (100/4), 100/1.5, 100/2.5)
-//         // C2D.ellipse(mouse.x + (100/3), mouse.y - 20, 100/9)
-//         // C2D.ellipse(mouse.x - (100/3), mouse.y - 20, 100/9)
-//         // one_colors(o_fill)
-//         // C2D.ellipse(mouse.x , mouse.y + 100/5, 70, 35)
-//         // one_color(o_stroke)
-//         // C2D.ellipse(mouse.x + (100/3), mouse.y - 20, 100/10, 100/5)
-//         // C2D.ellipse(mouse.x - (100/3), mouse.y - 20, 100/10, 100/5)
-//         // original_colors()
-        
-//       }
-//     }
-//   }
-// }
+ 
