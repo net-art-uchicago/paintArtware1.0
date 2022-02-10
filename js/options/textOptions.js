ac4fc23d92f50b07143be1af4eb4640f5c635d75
bucketteOfIvy <ashewimer@gmail.com>
2022-02-09 22:47:37 -0600

    textoptions now is cursed

--- js/options/textOptions.js
+++ js/options/textOptions.js
@@ -7,12 +7,58 @@ window.options.textOptions = {
 
   run: function () {
     const state = window.options.textOptions.state
+    const sc = document.createElement('script')
+    sc.innerHTML = `
+      // takes a list and "scootches" the elements
+      function scootch (c) {
+        const r = []
+        c.forEach(col => {
+          r.push(col)
+        })
+        return r
+      }
+
+      // takes an element e and list of colors c and makes the background cursed
+      function setBackgroundRainbow (e, c) {
+        var b = \`linear-gradient(\${Math.random() * 360}deg,\`
+        for (let i = 0; i < c.length; i++) {
+          if (i + 1 !== c.length) b = b + c[i] + \` \${i * 10}%,\`
+          else b = b + c[i] + \` \${i * 10}%\`
+        }
+        b = b + ')'
+        e.style.background = b
+        console.log(b)
+      }
+
+      // what if text-options had a painful background
+      // kudos to Bartek on stack-overflow for these colors
+      const textoption = document.querySelector('section[id="tool-options"]')
+      let colors = ['rgba(255, 0, 0, 1)',
+        'rgba(255, 154, 0, 1)',
+        'rgba(208, 222, 33, 1)',
+        'rgba(79, 220, 74, 1)',
+        'rgba(63, 218, 216, 1)',
+        'rgba(47, 201, 226, 1)',
+        'rgba(28, 127, 238, 1)',
+        'rgba(95, 21, 242, 1)',
+        'rgba(186, 12, 248, 1)',
+        'rgba(251, 7, 217, 1)',
+        'rgba(255, 0, 0, 1)']
+      // only once, for debugging purposes
+      setBackgroundRainbow(textoption, colors)
+      //setInterval(setBackgroundRainbow, 100, textoption, colors)
+      setInterval(function () {
+        colors = scootch(colors)
+      }, 100)
+    `
+    document.body.appendChild(sc)
     const div = document.createElement('div')
     div.innerHTML = `
       <style>
+
        .text-options {
           display: grid;
-          grid-template-columns: 450px;
+          grid-template-columns: repeat(50, 1fr);
           grid-template-rows: 1fr 1fr;
           grid-template-areas:
           "top"
