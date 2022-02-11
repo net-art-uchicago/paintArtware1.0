9b2baad3a91d5e0f2a51796e79d242c70245e2f0
silviachie <88081257+silviachie@users.noreply.github.com>
2022-02-11 13:09:46 -0600

    JS style updates

--- js/tools/smile.js
+++ js/tools/smile.js
@@ -22,18 +22,18 @@ window.tools.smile = {
         fill1 = C2D.fill
         stroke1 = C2D.stroke
         width = (C2D.ctx.lineWidth) * 5
-        C2D.change_colors(fill1, fill1)
+        C2D.changeColors(fill1, fill1)
         C2D.ellipse(mouse.x, mouse.y, width * 1.05)
-        C2D.change_colors(stroke1, stroke1)
+        C2D.changeColors(stroke1, stroke1)
         C2D.ellipse(mouse.x, mouse.y, width) // head
-        C2D.change_colors(fill1, fill1)
+        C2D.changeColors(fill1, fill1)
         C2D.ellipse(mouse.x, mouse.y + (width / 5), width / 1.8, width / 1.9) // inner mouth
-        C2D.change_colors(stroke1, stroke1)
+        C2D.changeColors(stroke1, stroke1)
         C2D.ellipse(mouse.x, mouse.y, (width * 0.7), (width * 0.4)) // outer mouth
-        C2D.change_colors(fill1, fill1)
+        C2D.change_Colors(fill1, fill1)
         C2D.ellipse(mouse.x + (width / 2.75), mouse.y - (width / 5), width / 13, width / 7) // eyes
         C2D.ellipse(mouse.x - (width / 2.75), mouse.y - (width / 5), width / 13, width / 7)
-        C2D.change_colors(stroke1, fill1)
+        C2D.changeColors(stroke1, fill1)
       }
     }
   }
