7e0f443293d1e7ff774fcbc03bf8b605f0bcf683
Ethan Croitoru <ecroitoru@uchicago.edu>
2022-02-14 16:23:18 -0600

    add functions sentiment lib

--- js/libs/sentiment.js
+++ js/libs/sentiment.js
@@ -1,5 +1,15 @@
 class Sent {
+  static async loadModels () {}
 
+  static textPredict () {}
+
+  static readFaceExpression () {}
+
+  static getMood () {}
+
+  static getTopSong () {}
+
+  static getColor () {}
 }
 
 window.Sent = Sent
