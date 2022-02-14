d16e190e9c1dd7220a87126d28427411c940c71b
silviachie <88081257+silviachie@users.noreply.github.com>
2022-02-14 14:49:50 -0600

    updated color function, icon name, and JS style

--- js/tools/smile.js
+++ js/tools/smile.js
@@ -1,6 +1,7 @@
+/* global C2D */
 window.tools.smile = {
   name: ':)',
-  icon: '/images/smile.png',
+  icon: '/images/smile-icon.png',
   state: {
     selected: false,
     mousePressed: false
@@ -16,24 +17,31 @@ window.tools.smile = {
     },
     mousemove: function (e) {
       const state = window.tools.smile.state
-
       if (state.selected && state.mousePressed) {
         const mouse = C2D.eventToMouse(e)
-        fill1 = C2D.fill
-        stroke1 = C2D.stroke
-        width = (C2D.ctx.lineWidth) * 5
-        C2D.changeColors(fill1, fill1)
+
+        const fill1 = C2D.fill
+        const stroke1 = C2D.stroke
+        const width = (C2D.ctx.lineWidth) * 5
+
+        C2D.setColors(fill1, fill1)
         C2D.ellipse(mouse.x, mouse.y, width * 1.05)
-        C2D.changeColors(stroke1, stroke1)
-        C2D.ellipse(mouse.x, mouse.y, width) // head
-        C2D.changeColors(fill1, fill1)
+
+        C2D.setColors(stroke1, stroke1)
+        C2D.ellipse(mouse.x, mouse.y, width) // face
+
+        C2D.setColors(fill1, fill1)
+        C2D.ellipse(mouse.x, mouse.y + (width / 5), width / 1.8, width / 1.9) // innermouth
         C2D.ellipse(mouse.x, mouse.y + (width / 5), width / 1.8, width / 1.9) // inner mouth
-        C2D.changeColors(stroke1, stroke1)
+
+        C2D.setColors(stroke1, stroke1)
         C2D.ellipse(mouse.x, mouse.y, (width * 0.7), (width * 0.4)) // outer mouth
-        C2D.change_Colors(fill1, fill1)
+
+        C2D.setColors(fill1, fill1)
         C2D.ellipse(mouse.x + (width / 2.75), mouse.y - (width / 5), width / 13, width / 7) // eyes
         C2D.ellipse(mouse.x - (width / 2.75), mouse.y - (width / 5), width / 13, width / 7)
-        C2D.changeColors(stroke1, fill1)
+
+        C2D.setColors(fill1, stroke1)
       }
     }
   }
