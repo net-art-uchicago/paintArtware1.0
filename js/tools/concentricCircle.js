9b1d07d93d39d5ba4c464a40e7c76eb8b0874c2c
wzeng0 <75603464+wzeng0@users.noreply.github.com>
2022-02-14 13:43:28 -0600

    style changes

--- js/tools/concentricCircle.js
+++ js/tools/concentricCircle.js
@@ -10,7 +10,7 @@ window.tools.concentricCircle = {
     events: {
         mousedown: function (e) {
             const state = window.tools.concentricCircle.state
-            const mouse = C2D.eventToMouse(e);
+            const mouse = C2D.eventToMouse(e)
             state.mousePressed = true
             state.prevMouse = { x: mouse.x, y: mouse.y}
             state.prevColor = C2D.fill
@@ -25,12 +25,12 @@ window.tools.concentricCircle = {
             const state = window.tools.concentricCircle.state;
             if (state.selected && state.mousePressed)
             {
-                const mouse = C2D.eventToMouse(e);
-                let X = state.prevMouse.x;
-                let Y = state.prevMouse.y;
-                let R = Math.abs(X - mouse.x);
-                C2D.fill = "rgba(255, 255, 255, 0.1)";
-                C2D.ellipse(X, Y, R);
+                const mouse = C2D.eventToMouse(e)
+                let X = state.prevMouse.x
+                let Y = state.prevMouse.y
+                let R = Math.abs(X - mouse.x)
+                C2D.fill = "rgba(255, 255, 255, 0.1)"
+                C2D.ellipse(X, Y, R)
             }
         },
     }
