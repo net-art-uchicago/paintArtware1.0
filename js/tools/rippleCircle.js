df7fcf87a9e2542d983f367b5463953471f046ce
wzeng0 <75603464+wzeng0@users.noreply.github.com>
2022-02-21 14:36:55 -0600

    semi ripple circle code that does not continue when another circle gets started

--- js/tools/rippleCircle.js
+++ js/tools/rippleCircle.js
@@ -7,30 +7,37 @@ window.tools.rippleCircle = {
     mousePressed: false,
     prevColor: null,
     timer: null,
-    radius: 0
+    radius: 0,
+    rippleEffect: true
   },
   events: {
-    mousedown: function (e) {
+    mousedown: function () {
       const state = window.tools.rippleCircle.state
-      const mouse = C2D.eventToMouse(e)
       state.mousePressed = true
       state.prevColor = C2D.fill
-      let interval = 0
-      if (state.selected) {
+      state.radius = 0
+      state.rippleEffect = true
+      clearInterval(state.timer)
+    },
+    mouseup: function (e) {
+      const state = window.tools.rippleCircle.state
+      const mouse = C2D.eventToMouse(e)
+      let interval = 0;
+      if (state.selected && state.rippleEffect) {
         state.timer = setInterval(() => {
           interval += 2
           state.radius = state.radius + 5 * interval
           C2D.fill = 'rgba(255, 255, 255, .5)'
           C2D.ellipse(mouse.x, mouse.y, state.radius)
-        }, 1000)
+          if (interval == 30) {
+            state.rippleEffect = false
+            clearInterval(state.timer)
+          }
+          console.log(interval)
+        }, 250)
       }
-    },
-    mouseup: function () {
-      const state = window.tools.rippleCircle.state
       state.mousePressed = false
       C2D.fill = state.prevColor
-      clearInterval(state.timer)
-      state.radius = 0
     }
   }
 }
