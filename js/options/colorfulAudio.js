5a0867ce87825c3ccd92fc8c939628c971db9205
lrmantovani10 <lrmantovani10@gmail.com>
2022-02-25 18:06:35 -0600

    Improving option

--- js/options/colorfulAudio.js
+++ js/options/colorfulAudio.js
@@ -2,7 +2,8 @@
 window.options.colorfulAudio = {
   name: 'colorful audio',
   state: {
-    selected: false
+    selected: false,
+    randomNum: 0
   },
   run: function () {
     const div = document.createElement('div')
@@ -34,48 +35,67 @@ window.options.colorfulAudio = {
         <audio id="savedAudio" controls></audio>
       </div>
     `
-    function colorfulSounds () {
+
+    function soundtoColor(){
       const savedSound = document.getElementById('savedAudio')
-      const randomNum = Math.random()
+      const randomNum = window.options.colorfulAudio.state.randomNum
+      let time = savedSound.currentTime
+      const duration = savedSound.duration
+      const pixels = C2D.getPixels()
+      const screenWidth = C2D.width
+      const portion = (C2D.height / 100)
 
-      savedSound.onplay = () => {
-        const pixels = C2D.getPixels()
-        const screenWidth = C2D.width
-        const portion = (C2D.height / 100)
-        let partialRow = 1
-        let accumulator = 1
-        let currentColor = 3
-        let time = savedSound.currentTime
+      let partialRow = 1
+      let accumulator = 1
+      let currentColor = 3
 
-        if (time > 0 < 1) { time = 1 }
+      if (time > 0 < 1) { time = 1 }
 
-        const soundMultiplier = (savedSound.volume *
-          time / savedSound.duration)
+      const soundMultiplier = (savedSound.volume *
+        time / duration)
 
-        for (let i = 0; i < pixels.length; i++) {
-          if (i >= (screenWidth * partialRow)) {
-            partialRow++
-            if (accumulator >= portion) {
-              currentColor += 4
-              accumulator = 0
-            }
-            accumulator++
+      for (let i = 0; i < pixels.length; i++) {
+        if (i >= (screenWidth * partialRow)) {
+          partialRow++
+          if (accumulator >= portion) {
+            currentColor += 4
+            accumulator = 0
           }
-
-          const colorList = [parseInt(currentColor * soundMultiplier),
-            parseInt(randomNum * currentColor * soundMultiplier),
-            parseInt((randomNum + 0.1) * currentColor * soundMultiplier)]
-
-          for (let i = 0; i < colorList.length; i++) {
-            if (colorList[i] > 255) { colorList[i] = 255 } else if (colorList[i] < 0) { colorList[i] = 0 }
-          }
-
-          pixels[i].r = colorList[0]
-          pixels[i].g = colorList[1]
-          pixels[i].b = colorList[2]
+          accumulator++
         }
 
-        C2D.setPixels(pixels)
+        const colorList = [parseInt(currentColor * soundMultiplier),
+          parseInt(randomNum * currentColor * soundMultiplier),
+          parseInt((1 - randomNum) * currentColor * soundMultiplier)]
+
+        for (let i = 0; i < colorList.length; i++) {
+          if (colorList[i] > 255) { 
+            colorList[i] = 255 
+            window.options.colorfulAudio.state.randomNum = Math.random()
+          } 
+          else if (colorList[i] < 0) { 
+            colorList[i] = 0
+            window.options.colorfulAudio.state.randomNum = Math.random()
+          }
+        }
+
+        pixels[i].r = colorList[0]
+        pixels[i].g = colorList[1]
+        pixels[i].b = colorList[2]
+        pixels[i].a = 255
+      }
+
+      C2D.setPixels(pixels)
+
+      if(!savedSound.paused){
+        setTimeout(soundtoColor, 400)
+      }
+    }
+
+    function colorSounds () {
+      const savedSound = document.getElementById('savedAudio')
+      savedSound.onplaying = () => {
+        soundtoColor()
       }
     }
 
@@ -88,10 +108,11 @@ window.options.colorfulAudio = {
         btn.innerHTML = state1
         window.options.colorfulAudio.state.selected = false
         UserMedia.recordAudio(null, null, false)
-        colorfulSounds()
+        colorSounds()
       } else {
         btn.innerHTML = state2
         window.options.colorfulAudio.state.selected = true
+        window.options.colorfulAudio.state.randomNum = Math.random()
         UserMedia.recordAudio('inputAudio', 'savedAudio', true)
       }
     })
