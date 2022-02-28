3156418c0d3adcd6d949e60dd6e660910ab04a0c
lnealer <lnealer@codeyourdreams.org>
2022-02-28 10:43:59 -0600

    Revert "readFacialExpression"
    This reverts commit 7c7bf67ce11f15291ce2bf1825d94d0699d39a15.

--- js/libs/sentiment.js
+++ js/libs/sentiment.js
@@ -1,39 +1,9 @@
-/* global faceapi */
 class Sent {
-  static async loadModels () {
-    // load the ml models
-    await faceapi.loadFaceExpressionModel('/js/assets/models')
-    await faceapi.loadSsdMobilenetv1Model('/js/assets/models')
-  }
+  static async loadModels () {}
 
   static textPredict () {}
 
-  static async readFacialExpression (img, verbose = false) {
-    // read the facial expression of input
-    const extractPrimaryEmotion = (expressionProbability) => {
-      // get the emotion of max probability from the object
-      const emotions = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
-      let maxValue = -1
-      let primaryEmotion = ''
-
-      emotions.forEach(e => {
-        if (expressionProbability[e] > maxValue) {
-          maxValue = expressionProbability[e]
-          primaryEmotion = e
-        }
-      })
-      return primaryEmotion
-    }
-
-    // detect expression
-    const output = await faceapi.detectSingleFace(img).withFaceExpressions()
-    const expressions = output.expressions
-    if (verbose) {
-      // return full object
-      return expressions
-    }
-    return extractPrimaryEmotion(expressions)
-  }
+  static readFaceExpression () {}
 
   static getMood () {}
 
