7cca33d4806055fb917ef60a08c9ee4e17b9e073
Shayna Kaushal <shaynakaushal@gmail.com>
2022-10-11 13:37:08 -0500

    bigger change

--- js/filters/dark.js
+++ js/filters/dark.js
@@ -6,14 +6,14 @@ window.filters.dark = {
       const imageData = app.ctx.getImageData(0, 0, app.canvas.width, app.canvas.height)
       const data = imageData.data
       for (let i = 0; i < data.length; i += 4) {
-        if (data[i] - 10 <= 0){ data[i] = 0 }
-        else { data[i] -= 10 }
+        if (data[i] - 20 <= 0){ data[i] = 0 }
+        else { data[i] -= 20 }
 
-        if (data[i+1] - 10 <= 0){ data[i+1] = 0 }
-        else { data[i+1] -= 10 }
+        if (data[i+1] - 20 <= 0){ data[i+1] = 0 }
+        else { data[i+1] -= 20 }
 
-        if (data[i+2] - 10 <= 0){ data[i+2] = 0 }
-        else { data[i+2] -= 10 }
+        if (data[i+2] - 20 <= 0){ data[i+2] = 0 }
+        else { data[i+2] -= 20 }
       }
       app.ctx.putImageData(imageData, 0, 0)
     }
